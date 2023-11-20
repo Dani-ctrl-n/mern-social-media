ec3e404594d97657c2a9d536ef11b6d50921001f
Dani-ctrl-n <mugabodaniel26@gmail.com>
2023-11-20 18:53:48 +0200

    create login functionality

--- server/index.js
+++ server/index.js
@@ -8,6 +8,7 @@ import multer from "multer";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
+import authRoutes from "./routes/auth.js";
 import { register } from "./controllers/auth.js";
 
 const __filename = fileURLToPath(import.meta.url);
@@ -34,6 +35,8 @@ const storage = multer.diskStorage({
 const upload = multer({ storage });
 
 app.post("/auth/register", upload.single("picture"), register);
+
+app.use("/auth", authRoutes);
 
 // const PORT = process.env.PORT || 6001;
 // mongoose
